e5251c7c5de19dd58c6a7a8f38f8075b6ece2b4d
Nicholas Liem <13521135@std.stei.itb.ac.id>
2024-04-20 16:04:04 +0700

    clean up

--- Utils/LossFunction.py
+++
@@ -1,9 +0,0 @@
-class LossFunction:
-    @staticmethod
-    def loss_rsl(self, x):
-        raise Exception("TODO: Implement this")
-    
-    @staticmethod
-    def loss_softmax(self, x):
-        raise Exception("TODO: Implement this")
-
